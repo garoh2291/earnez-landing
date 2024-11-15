59bb361835c0387ead0be8a54aaddc00f0f604d1
garoh2291 <garoh2291@gmail.com>
2024-11-15 17:04:53 +0400

    Fix favicon

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -17,7 +17,7 @@ export const metadata: Metadata = {
   description:
     "Turn your Android phone into an automatic income stream with EarnEZ. Earn €0.01-0.03 per SMS test message. No effort required, fast payouts, and complete privacy protection.",
   icons: {
-    icon: "./favicon.ico",
+    icon: "../favicon.ico",
   },
 };
 
