734c97b908e2dfcb89690e3c948c2b50e00fd92a
garoh2291 <garoh2291@gmail.com>
2024-11-15 18:34:14 +0400

    Fix build issue

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,7 +1,7 @@
 "use client";
 import Image from "next/image";
 import Link from "next/link";
-import React, { use } from "react";
+import React from "react";
 import { HiHeart } from "react-icons/hi";
 import { useTranslations } from "next-intl";
 
